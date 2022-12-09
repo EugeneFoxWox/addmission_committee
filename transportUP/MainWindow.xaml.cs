4157811d68728241f766965d4fa62c1cbbb49b20
denischagin <denischaginnn@gmail.com>
2022-12-09 19:16:06 +0300

    Добавляет файл JSON, но проблемы с кодировкой

--- transportUP/MainWindow.xaml.cs
+++ transportUP/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,11 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using transportUP.DB;
 using transportUP.Pages;
+using System.Windows.Markup;
+using System.IO;
+using Microsoft.VisualBasic;
+using System.Collections.ObjectModel;
+using System.Text.Encodings.Web;
 
 namespace transportUP
 {
@@ -25,9 +32,9 @@ namespace transportUP
     {
         ApplicationContext db = new ApplicationContext();
         public MainWindow()
-        {
+        {   
             InitializeComponent();
-            Loaded += MainWindow_Loaded;
+            Loaded += MainWindow_Loaded;    
             Title = "Транспорт";
             employeesList.SelectionChanged += employeesList_SelectionChanged;
         }
@@ -98,6 +105,66 @@ namespace transportUP
             if (employee is null) return;
             db.Employees.Remove(employee);
             db.SaveChanges();
+        }
+
+        void OnComboboxTextChanged(object sender, RoutedEventArgs e)
+        {
+            //_CBFind.IsDropDownOpen = true;
+            // убрать selection, если dropdown только открылся
+            //var tb = (TextBox)e.OriginalSource;
+            //tb.Select(tb.SelectionStart + tb.SelectionLength, 0);
+            //CollectionView cv = (CollectionView)CollectionViewSource.GetDefaultView(_CBFind.ItemsSource);
+            //cv.Filter = s =>
+            //    ((string)s).IndexOf(_CBFind.Text, StringComparison.CurrentCultureIgnoreCase) >= 0;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            switch (comboBox.SelectedIndex)
+            {
+                case 0:
+                    DataContext = db.Employees.OrderBy(empl => empl.Surname).ToList();
+                    break;
+                case 1:
+                    DataContext = db.Employees.OrderByDescending(empl => empl.Surname).ToList();
+                    break;
+                case 2:
+                    DataContext = db.Employees.OrderBy(empl => empl.Name).ToList();
+                    break;
+                case 3:
+                    DataContext = db.Employees.OrderByDescending(empl => empl.Name).ToList();
+                    break;
+            }
+        }
+
+        private void Find_Loaded(object sender, RoutedEventArgs e)
+        {
+            //var employees = db.Employees.Local.ToObservableCollection();
+            //employees.Select(empl => String.Format("{0} {1} {2}", empl.Surname, empl.Name, empl.Patronymic));
+            //_CBFind.DataContext = employees;
+
+        }
+
+        async private void Json_Click(object sender, RoutedEventArgs e)
+        {
+            db.Employees.Load();
+            ObservableCollection<Employee> _data = (db.Employees.Local.ToObservableCollection());
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All)
+            };
+            try
+            {
+                string json = JsonSerializer.Serialize(_data, options);
+                await using FileStream createStream = File.Create(@"C:\path.json");
+                await JsonSerializer.SerializeAsync(createStream, _data);
+                MessageBox.Show(@"Создался файл: C:\\path.json");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
